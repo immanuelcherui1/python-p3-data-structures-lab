dc50d22223fd25c6e7c691e25c878e900a7b95ef
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-03-07 01:43:38 +0300

    Done with Manipulation

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,29 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [foods["name"] for foods in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [foods for foods in spicy_foods if foods["heat_level"] >= 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for foods in spicy_foods: 
+        heat_level_emoji = "🌶" * foods["heat_level"]
+        print(f'{foods["name"]} ({foods["cuisine"]}) | Heat Level: {heat_level_emoji}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for foods in spicy_foods:
+        if foods["cuisine"] == cuisine:
+            return foods
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for foods in spicy_foods:
+        if foods["heat_level"] >= 5:
+            print(f'{foods["name"]} ({foods["cuisine"]}) | Heat Level: {"🌶" * foods["heat_level"]}')
 
 def get_average_heat_level(spicy_foods):
-    pass
+    return sum([foods["heat_level"] for foods in spicy_foods]) / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
